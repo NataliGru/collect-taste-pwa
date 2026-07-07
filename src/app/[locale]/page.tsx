e3b94baf0687183340ba93fe35aeeb14beda3cf1
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2026-07-07 19:55:13 +0300

    Add shadcn, theme colors, start change button variants

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,20 @@
 import { useTranslations } from 'next-intl';
 
+import { Button } from '@/shared';
+
 export default function Home() {
   const t = useTranslations();
 
   return (
     <div className='flex flex-row w-full justify-between gap-4 p-4 items-center'>
       Main page
+      <Button>default</Button>
+      <Button variant={'outline'}>outline</Button>
+      <Button variant={'secondary'}>secondary</Button>
+      <Button variant={'ghost'}>ghost</Button>
+      <Button variant={'destructive'}>destructive</Button>
+      <Button variant={'link'}>link</Button>
+      <Button variant={'accent'}>accent</Button>
     </div>
   );
 }
